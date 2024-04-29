9ad7825551eb041866fdbcad1c2874625eb2c43a
Juan <gurquiza8@gmail.com>
2024-04-29 18:05:53 -0500

    new form group for custom validations

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,9 +1,30 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './register-page.component.html',
   styles: ``
 })
 export class RegisterPageComponent {
+
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [ Validators.required ]],
+    email: ['', [ Validators.required ]],
+    username: ['', [ Validators.required ]],
+    password: ['', [ Validators.required, Validators.minLength(6) ]],
+    password2: ['', [ Validators.required ]]
+  })
+
+  constructor( private fb: FormBuilder ) {}
+
+  isValidField( field: string ) {
+
+  }
+
+  onSubmit():void {
+    this.myForm.markAllAsTouched();
+    console.log(this.myForm.value);
+
+  }
 
 }
